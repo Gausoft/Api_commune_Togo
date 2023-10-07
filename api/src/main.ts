006d8022ac7f5c7c0f5c9409b085d480265791d0
Gauthier EHOLOUM <thiergau691@gmail.com>
2023-10-07 18:10:13 +0000

    chore: add swagger doc

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,31 @@
+import { AppModule } from '@/app/app.module';
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  setupOpenApi(app);
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
+
+  Logger.log(`🚀 API is running on: http://localhost:${port}/doc`);
 }
+
 bootstrap();
+
+function setupOpenApi(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('🚀 Subdivisions administrative du Togo 🇹🇬')
+    .setDescription(
+      'Informations géographiques et démographiques détaillées sur les régions, villes, communes, préfectures, etc.',
+    )
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+}
